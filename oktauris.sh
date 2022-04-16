9c025493aa439461630c6c6682a37d6f96c1fc4f
Gridin94 <100672115+Gridin94@users.noreply.github.com>
2022-04-16 15:44:01 +0300

    add script

---
+++ oktauris.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+'curl --location --request PUT 'https://{{ OKTAURL }}//api/v1/apps/{{ OKTACLIENT }}' \
+--header 'Accept: application/json' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: SSWS {{ OKTAKEY }}' \
+--data-raw '{
+    "name": "oidc_client",
+    "label": "Node Weight Tracker",
+    "credentials": {
+        "oauthClient": {
+            "autoKeyRotation": true,
+            "client_id": "{{ OKTACLIENT }}",
+            "token_endpoint_auth_method": "client_secret_basic"
+        }
+    },
+    "settings": {
+        "oauthClient": {
+            "redirect_uris": [
+                "http://{{ HOSTIP }}:8080/authorization-code/callback"
+            ],
+            "post_logout_redirect_uris": [
+                "http://{{ HOSTIP }}:8080/logout"
+            ],
+            "response_types": [
+                "code"
+            ],
+            "application_type": "web",
+            "consent_method": "REQUIRED",
+            "issuer_mode": "DYNAMIC"
+        }
+    }
+}'
