36ed4b1d8510f57e1f85c4ef6a9388bab7c8f4d5
Gridin94 <100672115+Gridin94@users.noreply.github.com>
2022-04-16 18:42:14 +0300

    1234

--- oktauris.sh
+++ oktauris.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-OKTAURL = $1
-OKTACLIENT = $2
-OKTAKEY = $3
-HOSTIP = $4
+# OKTAURL = $1
+# OKTACLIENT = $2
+# OKTAKEY = $3
+# HOSTIP = $4
 
-curl --location --request PUT $OKTAURL'//api/v1/apps/'$OKTACLIENT \
+curl --location --request PUT $1'//api/v1/apps/'$2 \
 --header 'Accept: application/json' \
 --header 'Content-Type: application/json' \
---header 'Authorization: SSWS $OKTAKEY' \
+--header 'Authorization: SSWS '$3 \
 --data-raw '{
     "name": "oidc_client",
     "label": "Node Weight Tracker",
     "credentials": {
         "oauthClient": {
             "autoKeyRotation": true,
-            "client_id": '"$OKTACLIENT"',
+            "client_id": '"$2"',
             "token_endpoint_auth_method": "client_secret_basic"
         }
     },
     "settings": {
         "oauthClient": {
             "redirect_uris": [
-                "http://'$HOSTIP':8080/authorization-code/callback"
+                "http://'$4':8080/authorization-code/callback"
             ],
             "post_logout_redirect_uris": [
-                "http://'$HOSTIP':8080/logout"
+                "http://'$4':8080/logout"
             ],
             "response_types": [
                 "code"
